cf007442566dd71cc783b79a9c14667b313827a5
Felipe <lamediatricolor@gmail.com>
2026-04-08 16:22:37 -0500

    ft: implementando configuracion de open api

---
+++ backend/src/main/java/com/veterinaria/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.veterinaria.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String schemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sistema Veterinario API")
+                        .version("1.0.0")
+                        .description("API REST para sistema de gestión de clínica veterinaria"))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components()
+                        .addSecuritySchemes(schemeName, new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
